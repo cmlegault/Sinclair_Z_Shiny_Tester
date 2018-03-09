f2383b2d56300cf94b4b0ee77ec00bfd2121a66f
cmlegault <chris.legault@noaa.gov>
2018-03-09 13:52:13 -0500

    add Introduction page

--- app.r
+++ app.r
@@ -90,7 +90,29 @@ give.n <- function(x){
 # Define UI for application that draws a histogram
 ui <- navbarPage("Sinclair Z Shiny Tester",  
                  
-   # Sidebar  
+   tabPanel("Introduction",
+            sidebarLayout(
+              sidebarPanel(
+                fluidRow(
+                  column(12,
+                         h2("Welcome"),
+                         br(),
+                         p("This Shiny app is designed to let you explore the performance of the Sinclair total mortality (Z) estimator using simulated data that you create. The Sinclair Z approach is similar to a catch curve, except that it includes multiple cohorts from adjacent years and estimates a single total mortality value for the group of cohorts (a common slope with different intercepts for each cohort). A moving window of 3 to 6 years is used and you decide which ages to include within each window. Diagnostics are provided to allow you to see the consequences of breaking the assumptions of constant Z within a window and full selectivity for all selected ages, as well as the influence of sampling variability. The interactive nature of the app allows quick feedback as you make a change in the data or options. Pop up windows provide information about the inputs and plots as you roll the mouse over them."),
+                         br(),
+                         p("Have fun!")
+                         )
+                )
+              ),
+              mainPanel(
+                h3("Data Creator"),
+                p("This is where you create the data by moving sliders and selecting options that define the population, fishery, and survey characteristics. You have the ability to make the fishery and survey selectivities logistic, domed, or full at all ages. You also determine how the fishing mortality changes over time (M is assumed equal to 0.2 for all years and ages, of course). You can add noise about the trend in F over time, including autocorrelation in the noise if you wish. The randomg number generator (RNG) seed value choice allows you to return to a single setting or explore different age ranges for estimating Z on a fixed data set."),
+                br(),
+                h3("Estimate Z"),
+                p("This is where you make the choice of which ages to use in the estimation of Z. Use the Diagnostic tab to see if the residuals are centered on zero. A partially selected age can produce a distribution of residuals that does not overlap the zero line. The number of years in the moving windows can be changed from the default value of 4 to examine the impact on estimates and diagnostics. Unlike the real world, the Z estimate plot shows the actual Z trend in red, so you can see how well the estimator performed. This should help you understand how much to pay attention to individual years using the Sinclair Z estimator. The individual fits can be paged through using the four buttons (display first, next, previous, and last fit), so that you can look at unusual or typical year windows in the time series of Z estimates.")
+              )
+            )
+   ),
+   
    tabPanel("Data Creator",
             sidebarLayout(
                sidebarPanel(
@@ -217,94 +239,94 @@ ui <- navbarPage("Sinclair Z Shiny Tester",
                                     "Corellation in random deviates for F from one year to the next",
                                     "right")
                           )
-                 )
-                   ),
+                   )
+               ),
                    
-                            mainPanel(
-                              plotOutput("selectivityPlot"),
-                              plotOutput("ZvaluesPlot"),
-                              bsTooltip("selectivityPlot",
-                                        "Shows selectivity at age for the fishery and survey, note the oldest age is not a plus group",
-                                        "left"),
-                              bsTooltip("ZvaluesPlot",
-                                        "Shows the true Z values at age over time as blue-black dots for the age range used in the Z estimation only, the red line is the average value over this age range (also shown in the Z estimates plot in the Estimate Z tab). Note the y-axis adjusts to the range of values and that M is always 0.2",
-                                        "left")
-                            )
-                          )
-                 ),
+               mainPanel(
+                  plotOutput("selectivityPlot"),
+                  plotOutput("ZvaluesPlot"),
+                  bsTooltip("selectivityPlot",
+                            "Shows selectivity at age for the fishery and survey, note the oldest age is not a plus group",
+                            "left"),
+                  bsTooltip("ZvaluesPlot",
+                            "Shows the true Z values at age over time as blue-black dots for the age range used in the Z estimation only, the red line is the average value over this age range (also shown in the Z estimates plot in the Estimate Z tab). Note the y-axis adjusts to the range of values and that M is always 0.2",
+                            "left")
+               )
+            )
+   ),
                  
-                 tabPanel("Estimate Z",
-                          sidebarLayout(
-                            sidebarPanel(
-                              sliderInput("ageInput", 
-                                          label = "Age Range", 
-                                          min = 1, 
-                                          max = 30, 
-                                          value = c(4,10)),
-                              
-                              bsTooltip("ageInput",
-                                        "Move these sliders to determine which ages are used in the Sinclair Z estimation",
-                                        "right"),
-                              
-                              sliderInput("nwSelection",
-                                          label = "Number of Years in Moving Window",
-                                          min = 3,
-                                          max = 6,
-                                          step = 1,
-                                          value = 4),
-                              
-                              bsTooltip("nwSelection",
-                                        "Width of moving window used in Sinclair Z estimation. Default value used in publication is 4",
-                                        "right"),
-                              
-                              actionButton("firstfit",
-                                           label = "Display First Fit"),
-                              
-                              bsTooltip("firstfit",
-                                        "These buttons cycle through the plots in the Fit tab",
-                                        "right"),
-                              
-                              actionButton("previousfit",
-                                           label = "Display Previous Fit"),
-                              
-                              actionButton("nextfit",
-                                           label = "Display Next Fit"),
-                              
-                              actionButton("lastfit",
-                                           label = "Display Last Fit"),
-                              
-                              br(),
-                              br(),
-                              h4("Reference"),
-                              p("Sinclair, A.F. 2001. Natural mortality of cod (Gadus morhua) in the Southern Gulf of St Lawrence. ICES Journal of Marine Science. 58: 1-10. https://doi.org/10.1006/jmsc.1999.0490")
-                              
-                            ),
-                            
-                            # Main panel (plots)
-                            mainPanel(
-                              tabsetPanel(
-                                tabPanel("Data", plotOutput("dataPlot")),
-                                tabPanel("Z estimates", plotOutput("ZPlot")),
-                                tabPanel("Diagnostic", plotOutput("diagnosticPlot")),
-                                tabPanel("Fits", plotOutput("fitsPlot")),
-                                bsTooltip("dataPlot",
-                                          "Survey catch at age and year for the ages selected",
-                                          "left"),
-                                bsTooltip("ZPlot",
-                                          "Estimates of total mortality (points) with 90% confidence intervals (vertical lines) and true value from Data Creator (red line)",
-                                          "left"),
-                                bsTooltip("diagnosticPlot",
-                                          "Box and whisker plot of residuals from all regressions (shown in Fits plots) for each age. Numbers denote the number of residuals at age (can differ due to zero catch at age for some cohorts)",
-                                          "left"),
-                                bsTooltip("fitsPlot",
-                                          "One moving window regression with common total mortality estimate (negative of the slope) and different intercepts for each cohort. Colors and symbols denote different cohorts). The y-axis for these plots is the natural logarithm of the survey catch at age for a year in the moving window. Use the buttons to cycle through different year windows",
-                                          "left")
-                                
-                                
-                              )
-                            )
-                          )
-                 )
+   tabPanel("Estimate Z",
+            sidebarLayout(
+               sidebarPanel(
+                  sliderInput("ageInput", 
+                              label = "Age Range", 
+                              min = 1, 
+                              max = 30, 
+                              value = c(4,10)),
+                  
+                  bsTooltip("ageInput",
+                            "Move these sliders to determine which ages are used in the Sinclair Z estimation",
+                            "right"),
+                  
+                  sliderInput("nwSelection",
+                              label = "Number of Years in Moving Window",
+                              min = 3,
+                              max = 6,
+                              step = 1,
+                              value = 4),
+                  
+                  bsTooltip("nwSelection",
+                            "Width of moving window used in Sinclair Z estimation. Default value used in publication is 4",
+                            "right"),
+                  
+                  actionButton("firstfit",
+                               label = "Display First Fit"),
+                  
+                  bsTooltip("firstfit",
+                            "These buttons cycle through the plots in the Fit tab",
+                            "right"),
+                  
+                  actionButton("previousfit",
+                               label = "Display Previous Fit"),
+                  
+                  actionButton("nextfit",
+                               label = "Display Next Fit"),
+                  
+                  actionButton("lastfit",
+                               label = "Display Last Fit"),
+                  
+                  br(),
+                  br(),
+                  h4("Reference"),
+                  p("Sinclair, A.F. 2001. Natural mortality of cod (Gadus morhua) in the Southern Gulf of St Lawrence. ICES Journal of Marine Science. 58: 1-10. https://doi.org/10.1006/jmsc.1999.0490")
+                  
+               ),
+               
+               # Main panel (plots)
+               mainPanel(
+                  tabsetPanel(
+                     tabPanel("Data", plotOutput("dataPlot")),
+                     tabPanel("Z estimates", plotOutput("ZPlot")),
+                     tabPanel("Diagnostic", plotOutput("diagnosticPlot")),
+                     tabPanel("Fits", plotOutput("fitsPlot")),
+                     bsTooltip("dataPlot",
+                               "Survey catch at age and year for the ages selected",
+                               "left"),
+                     bsTooltip("ZPlot",
+                               "Estimates of total mortality (points) with 90% confidence intervals (vertical lines) and true value from Data Creator (red line)",
+                               "left"),
+                     bsTooltip("diagnosticPlot",
+                               "Box and whisker plot of residuals from all regressions (shown in Fits plots) for each age. Numbers denote the number of residuals at age (can differ due to zero catch at age for some cohorts)",
+                               "left"),
+                     bsTooltip("fitsPlot",
+                               "One moving window regression with common total mortality estimate (negative of the slope) and different intercepts for each cohort. Colors and symbols denote different cohorts). The y-axis for these plots is the natural logarithm of the survey catch at age for a year in the moving window. Use the buttons to cycle through different year windows",
+                               "left")
+                     
+                     
+                  )
+               )
+        )
+   )
 )
 
 
@@ -560,7 +582,7 @@ server <- function(input, output) {
     mat <- select(mat, -c(YEAR))
     rownames(mat) <- years
     res <- calc_Sinclair_Z(mat, input$nwSelection)
-    ny <<- length(res$plot.year)
+    ny <- length(res$plot.year)
     my.col <- 1:100
     my.col[7] <- "blue"
     data <- res[[icount$fit]]
